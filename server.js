891698f32bc6de4e1a80d1ac05a723b8489b74c4
senichimaro <senichimaro@gmail.com>
2021-10-22 01:08:07 +0200

    config PORT to Heroku deployment

--- server.js
+++ server.js
@@ -26,5 +26,5 @@ async function getMovies(req,res) {
     else res.json([]);
   }
 
-const port = 3000
+const port = process.env.PORT || 8080
 app.listen(port, () => console.log(`Service Running at: ${port}`))
